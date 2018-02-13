57e3e85d1d4015df1c6662c466c7ca73c9e00067
salimtx7 <salimrahmani10@gmail.com>
2018-02-13 13:16:40 +0000

    starting openpms project

---
+++ src/main/java/ma/cndh/openpms/OpenpmsApplication.java
@@ -0,0 +1,12 @@
+package ma.cndh.openpms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenpmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpenpmsApplication.class, args);
+	}
+}
